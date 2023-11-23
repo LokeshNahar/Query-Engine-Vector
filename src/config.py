9ec5a77cb34966ed1859dfa5e52ea4e7f53d765c
LokeshNahar <n.lokesh@iitg.ac.in>
2023-11-23 17:53:52 +0530

    change src code directory

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ import os
 
 CODE_DIR = os.path.dirname(__file__)
 ROOT_DIR = os.path.dirname(CODE_DIR)
-DATA_DIR = os.path.join(ROOT_DIR, "Chaabi/Data") # data directory
+DATA_DIR = os.path.join(ROOT_DIR, "/Data") # data directory
 STATIC_DIR = os.path.join(ROOT_DIR, "static")
 
 QDRANT_URL = os.environ.get("QDRANT_URL", "http://localhost:6333/")
